a8766106c25ab52897328dfff364bf739787195c
M Deekshitha Reddy <59170996+deekshugayu2931@users.noreply.github.com>
2020-02-20 18:39:26 +0530

    Add files via upload

---
+++ epam.java/src/main/java/epam/java/MainHouseClass.java
@@ -0,0 +1,14 @@
+package epam.java;
+
+import java.io.IOException;
+
+public class MainHouseClass 
+	{
+		    public static void main(String args[]) throws IOException 
+		    {
+		    	HouseConstruction c = new HouseConstruction();
+		    	System.out.println("Cost of Construction of House");
+		    	System.out.println("-----------------------------");
+		    	c.costEstimation();
+		    }
+	}
